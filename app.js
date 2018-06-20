336813c7c7fbac48796d9abb4310c2722a20c066
Nguyen Huu Minh Dat <nmcntt1.1512099@gmail.com>
2018-06-21 00:44:05 +0700

    update

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var index = require('./routes/index');
-var users = require('./routes/users');
-var mongo = require('mongoose');
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var MongoStore = require('connect-mongo')(session);
+var validator = require('express-validator');
 
 var app = express();
-mongo.connect('mongodb://localhost:27017/food');
-app.use(express.static('public'));
+
+var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb://localhost:27017/food');
+require('./config/passport');
+
 // view engine setup
-app.engine('.hbs',exphbs({defaultLayout:'layout',extname:'.hbs'}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({
+    secret: 'topsecret',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+app.use(function(req, res, next) {
+   req.session.cookie.maxAge = 180 * 60 * 1000; // 3 hours
+    next();
+});
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+app.use('/user', userRoutes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,15 +63,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var food = require('../models/food');
+var Cart = require('../models/cart');
 
+router.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	food.find(function(err,docs)
@@ -10,9 +15,80 @@ router.get('/', function(req, res, next) {
 		var chunkSize=3;
 		for(var i=0; i<docs.length; i+=chunkSize)
 			foodChunk.push(docs.slice(i,i+chunkSize));
-		res.render('index', { title: 'Express', foods: foodChunk});
+		res.render('shop/index', { title: 'Express', foods: foodChunk});
 	});
   	
 });
 
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart.items : {});
+    
+    food.findById(productId, function (err, food) {
+        cart.add(food, food.id);
+        req.session.cart = cart;
+        res.redirect('/');
+    });
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+   if (!req.session.cart) {
+       return res.render('shop/shopping-cart', {foods: null});
+   } 
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {foods: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    var errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    
+    var stripe = require("stripe")(
+        "sk_test_fwmVPdJfpkmwlQRedXec5IxR"
+    );
+
+    stripe.charges.create({
+        amount: cart.totalPrice * 100,
+        currency: "usd",
+        source: req.body.stripeToken, // obtained with Stripe.js
+        description: "Test Charge"
+    }, function(err, charge) {
+        if (err) {
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+        var order = new Order({
+            user: req.user,
+            cart: cart,
+            address: req.body.address,
+            name: req.body.name,
+            paymentId: charge.id
+        });
+        order.save(function(err, result) {
+            req.flash('success', 'Successfully bought product!');
+            req.session.cart = null;
+            res.redirect('/');
+        });
+    }); 
+});
+
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
