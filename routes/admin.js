5b2ae99ede230498751ec7d26209c9e4f43c23d5
Nguyen Huu Minh Dat <nmcntt1.1512099@gmail.com>
2018-06-29 12:14:20 +0700

    update final v1.1

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 
 
-router.get('/admin', function (req, res, next) {
+router.get('/', function (req, res, next) {
+    var successMsg = req.flash('success')[0];
+        res.render('admin/dashboard', {title: 'Admin', successMsg: successMsg, noMessages: !successMsg});
+});
+
+router.get('/dashboard', function (req, res, next) {
     var successMsg = req.flash('success')[0];
         res.render('admin/dashboard', {title: 'Admin', successMsg: successMsg, noMessages: !successMsg});
 });
@@ -12,6 +17,11 @@ router.get('/admin', function (req, res, next) {
 router.get('/user', function (req, res, next) {
     var successMsg = req.flash('success')[0];
         res.render('admin/user', {title: 'Admin', successMsg: successMsg, noMessages: !successMsg});
+});
+
+router.get('/table', function (req, res, next) {
+    var successMsg = req.flash('success')[0];
+        res.render('admin/table', {title: 'Admin', successMsg: successMsg, noMessages: !successMsg});
 });
 
 module.exports = router;
